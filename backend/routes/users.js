c571c9df15da0e241c0f204078bb2d069f8db9e2
nelsonliang <nelsonliang02@gmail.com>
2020-08-13 18:40:26 -0400

    Confirmed CRUD works for exercises

---
+++ backend/routes/users.js
@@ -0,0 +1,20 @@
+const router = require('express').Router(); //Need express router because this is a route
+let User = require('../models/user.model');
+
+router.route('/').get((req, res) => {
+    User.find() //Mongoose finds all the users and is a promise
+    .then(users => res.json(users)) //Then it returns the users in json format 
+    .catch(err => res.status(400).json('Error: ' + err)); //If error occurs throws it 
+})
+
+router.route('/add').post((req, res) => {
+    const username = req.body.username;
+
+    const newUser = new User({username});
+
+    newUser.save() //Save the newly created exercise 
+        .then(() => res.json('User added!'))
+        .catch(err => res.status(400).json('Error: ' + err))
+})
+
+module.exports = router;
