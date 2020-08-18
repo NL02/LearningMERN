ec82b0a6bf46e227518296c230393e74b51b87c2
nelsonliang <nelsonliang02@gmail.com>
2020-08-17 22:15:43 -0400

    Created a navbar and links to seperate components

---
+++ src/components/create-user.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CreateUser extends Component {
+    render() {
+        return (
+            <div>
+                <a>You're on the Create User component</a>
+            </div>
+        )
+    }
+}
---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ExerciseList extends Component {
+    render() {
+        return (
+            <div>
+                <a>You're on the Exercises List component</a>
+            </div>
+        )
+    }
+}
